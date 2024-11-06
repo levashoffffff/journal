94966587cd186e6cc0a717e8db6e9f2f156cc4d9
LevashovArtur <artur.levashoff@gmail.com>
2024-11-06 15:52:23 +0300

    Добавил сайт

---
+++ javascript.js
@@ -0,0 +1,8 @@
+
+
+function show_hide(id) {
+    var blockid = document.getElementById(id);
+    if (blockid.style.display == "none")
+        blockid.style.display = "block";
+    else blockid.style.display = "none";
+} 
